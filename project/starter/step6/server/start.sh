c2bdd3a9109e4b7160a640ebd0c03fa3fb8fc695
lastcodedefender <lastcodedefender@gmail.com>
2024-10-31 03:16:07 +0900

    updated project step6

---
+++ project/starter/step6/server/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Function to handle termination signals
+term_handler() {
+  echo "Received termination signal. Exiting..."
+  exit 0
+}
+
+# Trap termination signals
+trap 'term_handler' INT TERM
+
+while true; do
+  /app/server
+done
